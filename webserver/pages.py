09261cb2e51a1424f18559cca3214098087cd2e4
Nathan LECAILLE <272859471+nathanlecaille-ops@users.noreply.github.com>
2026-08-25 11:35:19 +0200

    Add 'Adamante-Cyber' on the login screen

--- webserver/pages.py
+++ webserver/pages.py
@@ -134,7 +134,7 @@ login_body = """
                     <button>login</button>
                 </form>
               </div>
-              <h3 style="font-family:'roboto', sans-serif; font-size:14px; color:#ffffff;">Release: 2025-12-18</h3>
+              <h3 style="font-family:'roboto', sans-serif; font-size:14px; color:#ffffff;">Release: 2026-08-25 - <span style="color:red"> By Adamante-Cyber </span></h3>
             </div>
         </div>
     </body>
